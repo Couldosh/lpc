fc7831c16be84e3a38a6a2182d368df6cc9c4e7d
Fabrice SIMON <fabrice.kergaline.simon@gmail.com>
2024-06-21 09:28:00 +1100

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "192.168.1.5:8080"
+const API_URL = "http://192.168.1.5:8080"
 document.addEventListener('DOMContentLoaded', function() {
     const photoForm = document.getElementById('photoForm');
     const photoInput = document.getElementById('photoInput');
